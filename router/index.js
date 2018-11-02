7f0bd00d0006cda1211be4331dac5facf069dd28
lianjl <xiaolian951210@163.com>
2018-11-03 01:11:49 +0800

    用户信息更新

--- router/index.js
+++ router/index.js
@@ -3,12 +3,15 @@
  */
 const express = require('express');
 const md5 = require('blueimp-md5');
+const cookieParseer = require('cookie-Parser');
 const Users = require('../models/users');
 //获取路由
 const Router = express.Router;
 //创建路由对象
 const router = new Router();
 router.use(express.urlencoded({extended:true}));
+router.use(cookieParseer());
+//登陆
 router.post('/login', async (req, res) => {
 //1.获取用户数据
     const {username, password} = req.body;
@@ -24,6 +27,7 @@ router.post('/login', async (req, res) => {
     try{
       const data =  await Users.findOne({username, password:md5(password)});
         if(data){
+            res.cookie('userid', data.id, {maxAge: 1000 * 3600 * 24 *30})
             res.json({
                 "code": 0,
                 "data": {
@@ -48,6 +52,7 @@ router.post('/login', async (req, res) => {
 
 
 });
+//注册
 router.post('/register', async (req, res) => {
     //1. 获取用户数据
     const {username, password, type} = req.body;
@@ -98,6 +103,7 @@ router.post('/register', async (req, res) => {
     try{
        const data = await Users.findOne({username});
        if(data){
+           res.cookie('userid', data.id, {maxAge: 1000 * 3600 * 24 *30});
            res.json({
                "code": 1,
                "msg": "用户已存在"
@@ -120,4 +126,38 @@ router.post('/register', async (req, res) => {
         });
     }
 });
+// 更新用户信息的路由
+router.post('/update', (req, res) => {
+    // 从请求的cookie得到userid
+    const userid = req.cookies.userid;
+    // 如果不存在, 直接返回一个提示信息
+    if (!userid) {
+        return res.json({code: 1, msg: '请先登陆'});
+    }
+    // 存在, 根据userid更新对应的user文档数据
+    // 得到提交的用户数据
+    const user = req.body; // 没有_id
+    Users.findByIdAndUpdate({_id: userid}, user)
+        .then(oldUser => {
+            if (!oldUser) {
+                //更新数据失败
+                // 通知浏览器删除userid cookie
+                res.clearCookie('userid');
+                // 返回返回一个提示信息
+                res.json({code: 1, msg: '请先登陆'});
+            } else {
+                //更新数据成功
+                // 准备一个返回的user数据对象
+                const {_id, username, type} = oldUser;
+                //此对象有所有的数据
+                const data = Object.assign({_id, username, type}, user);
+                // 返回成功的响应
+                res.json({code: 0, data})
+            }
+        })
+        .catch(error => {
+            // console.error('登陆异常', error)
+            res.send({code: 3, msg: '网络不稳定，请重新试试~'})
+        })
+})
 module.exports = router;
